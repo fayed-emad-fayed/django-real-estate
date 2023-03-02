54a7b041c5604f322d82097abcebe5c48e1fd09e
spiderman232 <sspiderman548@gmail.com>
2023-03-02 22:44:49 +0200

    err: corrected str.description with str.capitalize

--- apps/properties/models.py
+++ apps/properties/models.py
@@ -70,7 +70,7 @@ class Property(TimeStampedUUIDModel):
         
     def save(self, *args, **kwargs):
         self.title = str.title(self.title)
-        self.description = str.description(self.description)
+        self.description = str.capitalize(self.description)
         self.ref_code = "".join(random.choices(string.ascii_uppercase + str.digits, k=10))
         super(Property, self).save(*args, *kwargs)
         
