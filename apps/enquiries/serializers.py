cb9f8bdbf92fa5e3cba8151aa0de80790b8c7ddf
spiderman232 <sspiderman548@gmail.com>
2023-03-02 19:43:15 +0200

    feat: add properties, enquiries models,serializers

---
+++ apps/enquiries/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+from .models import Enquiry
+
+
+class EnquirySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Enquiry
+        # __all__ in fields means iclude all fields
+        fields = "__all__"
+        
+        
